369d725c8e456fd9a7300286e9f702f48ed44cec
Qurbonbekov Sherzod <qurbonbekovsher2004@gmail.com>
2025-08-25 16:24:32 +0500

    myhomework

--- script.js
+++ script.js
@@ -21,8 +21,16 @@ switch (subscription) {
         console.log("all access ");
         break;
     case "enterprise":
-        console.log("vip");
+        console.log("vip-support");
         break;
     default:
         console.log("Tariff not found");
 }
+
+if (productStatus === "available" && (subscription === "pro" || subscription === "enterprise")) {
+    console.log("product is available");
+} else if (productStatus === "sale" && (subscription === "user" || subscription === "guest")) {
+    console.log("product is unavailable");
+}else if (productStatus === "out-of-stock");
+console.log("Good is not available");
+

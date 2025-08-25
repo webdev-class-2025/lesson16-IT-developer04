c004d2dd39288805e34b976728266a638e5d7ad1
Qurbonbekov Sherzod <qurbonbekovsher2004@gmail.com>
2025-08-25 16:09:32 +0500

    myhomework

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let role = "admin";
+let subscription = "free";
+let productStatus = "available";
 
+if (role === "admin") {
+    console.log("Access granted");
+} else if (role === "user") {
+    console.log("Access restricted");
+} else if (role === "guest") {
+    console.log("Only view");
+} else {
+    console.log("unknown user");  
+  }
+
+
+switch (subscription) {
+    case "free":
+        console.log("minimal functionality");
+        break;
+    case "pro":
+        console.log("all access ");
+        break;
+    case "enterprise":
+        console.log("vip");
+        break;
+    default:
+        console.log("Tariff not found");
+}
